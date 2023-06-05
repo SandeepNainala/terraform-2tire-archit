ea89474ff3f31da5cefc2a11a56891d13cb005cd
sandeepnaidu <nainala_sandeep@yahoo.com>
2023-06-05 19:06:04 +0530

    error corrected

--- vpc.tf
+++ vpc.tf
@@ -50,7 +50,7 @@ resource "aws_subnet" "public-subnet1" {
   vpc_id                          = aws_vpc.custom_vpc.id
   cidr_block                      = "10.0.1.0/24"
   availability_zone_id            = "us-east-1"
-  map_customer_owned_ip_on_launch = true
+  map_public_ip_on_launch         = true
 
   tags = {
     Name = "public-subnet1"
@@ -62,7 +62,7 @@ resource "aws_subnet" "public-subnet2" {
   vpc_id                          = aws_vpc.custom_vpc.id
   cidr_block                      = "10.0.1.0/24"
   availability_zone_id            = "us-east-1"
-  map_customer_owned_ip_on_launch = true
+  map_public_ip_on_launch         = true
 
   tags = {
     Name = "public-subnet2"
